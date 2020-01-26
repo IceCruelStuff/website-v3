3f1493c2bbbe0843de28ce179d3959d3dd83a81c
Nathaniel Fredericks <me@nathfreder.dev>
2020-01-26 09:39:01 -0400

    Extract phar

---
+++ pages/extract.tsx
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import * as PHAR from "phar";
+import { saveAs } from "file-saver";
+import Error from "../utils/Error";
+
+function Extract() {
+    const [ error, setError ] = useState(null);
+    const [ files, setFiles ] = useState([]);
+
+    const handleFileChange = (event) => setFiles(event.target.files);
+
+    const extractPhar = () => {
+        setError(null);
+        const reader = new FileReader();
+
+        reader.onload = async () => {
+            try {
+                const phar = new PHAR.Archive();
+
+                phar.loadPharData(new Uint8Array(reader.result as ArrayBuffer));
+
+                const data = await PHAR.ZipConverter.toZip(phar);
+
+                const zip = await data.generateAsync({
+                    type: 'uint8array',
+                });
+
+                saveAs(
+                        new Blob([zip], {
+                            type: 'application/zip',
+                        }),
+                        `${files[0].name
+                                .split('.')
+                                .slice(0, -1)
+                                .join('.')}.zip`,
+                );
+            } catch {
+                setError(Error.UnknownError)
+            }
+        };
+
+        reader.readAsArrayBuffer(files[0])
+    };
+
+    return (
+            <>
+                <label htmlFor="file">File</label>
+                <input id="file" type="file" onChange={handleFileChange} accept=".phar" />
+                <button onClick={extractPhar} disabled={files.length < 1}>Extract</button>
+            </>
+    )
+}
+
+export default Extract
